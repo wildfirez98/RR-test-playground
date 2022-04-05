c4bd4a12fa22fcacafcf11de63b6df1bcc647fea
wildfirez98 <james@areafamily.us>
2022-04-04 21:37:30 -0500

    Testing working!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("Test header renders with correct text", () => {
+  render(<App />)
+  const headerEl = screen.getByRole("heading")
+  expect(headerEl.textContent).toBe("Testing Playground")
+})
+
+test("Button changes color when clicked", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+
+  userEvent.click(colorBtn)
+
+  expect(colorBtn).toHaveStyle({ backgroundColor: "blue" })
+  expect(colorBtn.textContent).toBe("Change button color to green")
+})
+
+test("checkbox disables button when first clicked, then enables button on second click", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+  const checkbox = screen.getByRole("checkbox")
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeDisabled()
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeEnabled()
+})
